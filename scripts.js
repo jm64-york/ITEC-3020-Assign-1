509a33b7071449d1dedcd2144387fbe4ea091b26
Joshua <jm64@my.yorku.ca>
2025-07-30 17:35:18 -0400

    Working version of blog page.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 const blogList = document.getElementById('blog-list');
 
-fetch('../data/posts.json')
+fetch('./data/posts.json')
 .then(response => response.json())
 .then(posts => {
 posts.forEach(post => {
 const postElement = document.createElement('div');
 // manipulate postElement to show the content of the blog post with the specific style defined for it
+postName = document.createElement('h2');
+postContent = document.createElement('p');
+postName.insertAdjacentText("afterbegin", post.post);
+postContent.insertAdjacentText("afterbegin", post.content);
+postElement.append (postName);
+postElement.append (postContent);
+blogList.append(postElement);
 //add postElement as a child to blog list
 });
 })
